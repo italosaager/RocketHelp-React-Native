2d18d53980f23ad961ac9637d517bc3a45b1c667
italosaager <italosaagergmail.com>
2022-07-20 04:06:36 -0300

    Projeto finalizado. Foram alteradas as configurações para rodar de forma nativa pois era necessário para utilizar o firebase. Foram feitas as configurações do firebase e implementado ao projeto. Foram terminadas todas as telas utilizando auth do firebase para autenticar o usuário e useEffect para fazer o loading das telas durante os carregamentos. App adicionando novas solicitações ao banco de dados e sendo capaz de encerrar tais solicitações e mudando seus estados com useState.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,30 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { BaseNavigationContainer,} from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import { SignIn } from "../screens/Signin";
 import { AppRoutes } from "./app.routes";
+import { useState, useEffect } from "react";
+import { Loading } from "../components/Loading";
 
 export function Routes() {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(response => {
+            setUser(response);
+            setLoading(false);
+        } );
+        return subscriber;
+    },[]);
+
+    if(loading) {
+        return <Loading/>
+    }
+
     return(
-        <NavigationContainer>
-            < AppRoutes />
-        </NavigationContainer>
+        <BaseNavigationContainer>
+            {user ? <AppRoutes /> : < SignIn />}
+        </BaseNavigationContainer>
 
     )
 }
